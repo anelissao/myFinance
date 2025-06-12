9d5f8faf50795669703446905b3e5c739633196b
Anouar <anouarelissaoui20@gmail.com>
2025-06-12 17:02:12 +0100

    updating reltionships

--- app/Models/FinancialGoal.php
+++ app/Models/FinancialGoal.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class FinancialGoal extends Model
 {
-    //
+    protected $fillable = [
+        'user_id', 'title', 'target_amount', 'saved_amount', 'deadline'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
